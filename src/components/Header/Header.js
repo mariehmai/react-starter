8d0d59cc492fae29455649fb202bb6b09ff6ca9f
mariehmai <mai.mariehelene@gmail.com>
2018-10-15 23:15:51 +0200

    feat(Header): add animation on scroll menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { object, func } from 'prop-types'
+import React, { Component } from 'react'
+import { object } from 'prop-types'
 import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
 import Button from '@material-ui/core/Button'
@@ -8,36 +8,62 @@ import routes from '../../containers/App/routes'
 import logo from '../../assets/logo.svg'
 import menuIcon from '../../assets/menu.svg'
 
-const Header = ({ history, showListMenu }) => (
-  <div className="header">
-    <div className="header-app-container">
-      <img src={logo} className="header-app-logo" alt="app-logo" />
-      <h2 className="headline nav">
-        <FormattedMessage id="app.title" />
-      </h2>
-    </div>
-    <div className="header-nav-container">
-      {routes.map(r => (
-        <Button
-          className="nav"
-          key={`${r.name}-id`}
-          onClick={() => history.push(r.path)}
-        >
-          {r.name}
-        </Button>
-      ))}
-    </div>
-    <div className="header-nav-menu-icon">
-      <Button onClick={showListMenu}>
-        <img src={menuIcon} alt="" />
-      </Button>
-    </div>
-  </div>
-)
+class Header extends Component {
+  state = {
+    isTop: true
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const top = window.scrollY
+    if (top > 40) this.setState({ isTop: false })
+    else this.setState({ isTop: true })
+  }
+
+  render() {
+    const { history } = this.props
+    const { isTop } = this.state
+
+    return (
+      <div className="header">
+        <div className={`header-content ${!isTop && 'fixed'}`}>
+          <div className="header-app-container">
+            <img src={logo} className="header-app-logo" alt="app-logo" />
+            <h2 className="headline nav">
+              <FormattedMessage id="app.title" />
+            </h2>
+          </div>
+          <div className="header-nav-container">
+            {routes.map(r => (
+              <Button
+                className="nav"
+                key={`${r.name}-id`}
+                onClick={() => history.push(r.path)}
+              >
+                {r.name}
+              </Button>
+            ))}
+            <div className="header-nav-icon-container">
+              <Button onClick={() => {}}>
+                <img className="header-nav-icon" src={menuIcon} alt="" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 Header.propTypes = {
-  history: object.isRequired,
-  showListMenu: func.isRequired
+  history: object.isRequired
 }
 
 export default withRouter(Header)
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import routes from './routes'
 import Header from '../../components/Header'
+import headerSVG from '../../assets/header.jpg'
 
 const randomTitle = 'Title'
 const randomText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
@@ -9,10 +10,6 @@ const randomText = `Lorem Ipsum is simply dummy text of the printing and typeset
 const App = () => (
   <div className="app">
     <Header />
-    <div className="container">
-      <h1>{randomTitle}</h1>
-      <p>{randomText}</p>
-    </div>
     <Switch>
       {routes.map(r => (
         <Route
@@ -23,6 +20,11 @@ const App = () => (
         />
       ))}
     </Switch>
+    <img className="background" src={headerSVG} alt="" />
+    <div className="container">
+      <h1>{randomTitle}</h1>
+      <p>{randomText}</p>
+    </div>
   </div>
 )
 
