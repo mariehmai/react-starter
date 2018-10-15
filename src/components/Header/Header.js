85c4193cf4d9756ec4eeb8c2a62f4c46a913aae0
mhmai <mariehelene.mai@bnpparibasfortis.com>
2018-10-15 09:49:00 +0200

    feat(Header): add show/hide button for small screen; update .eslintrc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { object } from 'prop-types'
+import React from 'react'
+import { object, func } from 'prop-types'
 import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
 import Button from '@material-ui/core/Button'
@@ -8,43 +8,36 @@ import routes from '../../containers/App/routes'
 import logo from '../../assets/logo.svg'
 import menuIcon from '../../assets/menu.svg'
 
-class Header extends Component {
-  render() {
-    const { history } = this.props
-
-    return (
-      <div className="header">
-        <div className="header-app-container">
-          <img src={logo} className="header-app-logo" alt="app-logo" />
-          <h2 className="headline nav">
-            <FormattedMessage id="app.title" />
-          </h2>
-        </div>
-        <div className="header-nav-container">
-          {routes.map(r => (
-            <Button
-              className="nav"
-              key={`${r.name}-id`}
-              onClick={() => history.push(r.path)}
-            >
-              {r.name}
-            </Button>
-          ))}
-          <Button
-            className="header-nav-menu-icon"
-            key="icon-id"
-            onClick={this.toggleMenu}
-          >
-            <img className="header-nav-menu-icon" src={menuIcon} alt="" />
-          </Button>
-        </div>
-      </div>
-    )
-  }
-}
+const Header = ({ history, showListMenu }) => (
+  <div className="header">
+    <div className="header-app-container">
+      <img src={logo} className="header-app-logo" alt="app-logo" />
+      <h2 className="headline nav">
+        <FormattedMessage id="app.title" />
+      </h2>
+    </div>
+    <div className="header-nav-container">
+      {routes.map(r => (
+        <Button
+          className="nav"
+          key={`${r.name}-id`}
+          onClick={() => history.push(r.path)}
+        >
+          {r.name}
+        </Button>
+      ))}
+    </div>
+    <div className="header-nav-menu-icon">
+      <Button onClick={showListMenu}>
+        <img src={menuIcon} alt="" />
+      </Button>
+    </div>
+  </div>
+)
 
 Header.propTypes = {
-  history: object.isRequired
+  history: object.isRequired,
+  showListMenu: func.isRequired
 }
 
 export default withRouter(Header)
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,9 +3,16 @@ import { Switch, Route } from 'react-router-dom'
 import routes from './routes'
 import Header from '../../components/Header'
 
+const randomTitle = 'Title'
+const randomText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+
 const App = () => (
   <div className="app">
     <Header />
+    <div className="container">
+      <h1>{randomTitle}</h1>
+      <p>{randomText}</p>
+    </div>
     <Switch>
       {routes.map(r => (
         <Route
