0739de843300bf3be9603b968cea7c3b0055ee15
mariehmai <mai.mariehelene@gmail.com>
2018-10-15 07:30:36 +0200

    feat(Header): add menu button for small screens

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { object } from 'prop-types'
 import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
@@ -6,28 +6,42 @@ import Button from '@material-ui/core/Button'
 
 import routes from '../../containers/App/routes'
 import logo from '../../assets/logo.svg'
+import menuIcon from '../../assets/menu.svg'
 
-const Header = ({ history }) => (
-  <div className="header">
-    <div className="header-app-container">
-      <img src={logo} className="header-app-logo" alt="app-logo" />
-      <h2 className="headline nav">
-        <FormattedMessage id="app.title" />
-      </h2>
-    </div>
-    <div className="header-nav-container">
-      {routes.map(r => (
-        <Button
-          className="nav"
-          key={`${r.name}-id`}
-          onClick={() => history.push(r.path)}
-        >
-          {r.name}
-        </Button>
-      ))}
-    </div>
-  </div>
-)
+class Header extends Component {
+  render() {
+    const { history } = this.props
+
+    return (
+      <div className="header">
+        <div className="header-app-container">
+          <img src={logo} className="header-app-logo" alt="app-logo" />
+          <h2 className="headline nav">
+            <FormattedMessage id="app.title" />
+          </h2>
+        </div>
+        <div className="header-nav-container">
+          {routes.map(r => (
+            <Button
+              className="nav"
+              key={`${r.name}-id`}
+              onClick={() => history.push(r.path)}
+            >
+              {r.name}
+            </Button>
+          ))}
+          <Button
+            className="header-nav-menu-icon"
+            key="icon-id"
+            onClick={this.toggleMenu}
+          >
+            <img className="header-nav-menu-icon" src={menuIcon} alt="" />
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
 
 Header.propTypes = {
   history: object.isRequired
