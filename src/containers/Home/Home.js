69a7c0a64bb7924151855e089cb58b4b878d85b8
Jipe Mai <jipemai@MacBook-Pro-2.local>
2018-11-18 20:42:09 +0100

    feat(homepage): add grid on homepage

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,26 +1,21 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
+import { Grid, Image } from 'semantic-ui-react'
+
+const treeImg = require('../../assets/tree.jpg')
 
 const Home = () => 
   <div className='home'>
-    <Container>
-    <div class="ui segment">
-      <div class="ui two column very relaxed grid">
-        <div class="column">
-          <img src='../img/test.jpg' alt='' />
-          <p>Exploitation forestière</p>
-          <p></p>
-        </div>
-        <div class="column">
-          <p></p>
-          <p>Exploitation agricole</p>
-          <p></p>
-        </div>
-      </div>
-      <div class="ui vertical divider">
-      </div>
-    </div>
-    </Container>
+      <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column className='home-column'>
+              <Image src={treeImg} />
+            </Grid.Column>
+            <Grid.Column>
+              <Image src={treeImg} />
+            </Grid.Column>
+          </Grid.Row>
+      </Grid>
   </div>
 
 export default Home
