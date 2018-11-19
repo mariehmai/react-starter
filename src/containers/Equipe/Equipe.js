a40256be7c0c329541cc55146b2a4158f080895d
Jipe Mai <jipemai@MacBook-Pro-2.local>
2018-11-19 11:29:41 +0100

    feat(teampage): add title + center cards

--- src/containers/Equipe/Equipe.js
+++ src/containers/Equipe/Equipe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Card, Icon, Image } from 'semantic-ui-react'
+import { Container, Card, Image } from 'semantic-ui-react'
 import { FormattedMessage } from 'react-intl'
 
 const bigBro = require('../../assets/thibaudbonnevial.jpg')
@@ -7,41 +7,44 @@ const littleBro = require('../../assets/noebonnevial.jpg')
 const smith = require('../../assets/smith.jpg')
 
 const Team = () => (
-  <Card.Group>
-    <Card>
-      <Card.Content>
-        <Image src={bigBro} />
-        <Card.Header>Thibaud Bonnevial</Card.Header>
-        <Card.Meta>Noe's Bro</Card.Meta>
-        <Card.Description>Thibaud is a nice guy with long hair</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <p>Tree killer</p>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image src={littleBro} />
-        <Card.Header>Noe Bonnevial</Card.Header>
-        <Card.Meta>Thibaud's Bro</Card.Meta>
-        <Card.Description>Noe is a nice guy with short hair</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <p>Tree healer</p>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image src={smith} />
-        <Card.Header>Smith Stan</Card.Header>
-        <Card.Meta>Bonnevial's employee</Card.Meta>
-        <Card.Description>Smith is a patient man working with Bonnevial's bros</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <p>Tree lover</p>
-      </Card.Content>
-    </Card>
-  </Card.Group>
+  <Container>
+    <h2><FormattedMessage id='equipe.title' /></h2>
+    <Card.Group centered items>
+      <Card>
+        <Card.Content>
+          <Image src={bigBro} alt='' />
+          <Card.Header>Thibaud Bonnevial</Card.Header>
+          <Card.Meta>Noe's Bro</Card.Meta>
+          <Card.Description>Thibaud is a nice guy with long hair</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>Tree killer</p>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Card.Content>
+          <Image src={littleBro} alt='' />
+          <Card.Header>Noe Bonnevial</Card.Header>
+          <Card.Meta>Thibaud's Bro</Card.Meta>
+          <Card.Description>Noe is a nice guy with short hair</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>Tree healer</p>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Card.Content>
+          <Image src={smith} alt='' />
+          <Card.Header>Smith Stan</Card.Header>
+          <Card.Meta>Bonnevial's employee</Card.Meta>
+          <Card.Description>Smith is a patient man working with Bonnevial's bros</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>Tree lover</p>
+        </Card.Content>
+      </Card>
+    </Card.Group>
+  </Container>
 )
 
 export default Team
