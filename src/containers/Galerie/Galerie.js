ca77fc50d9695387bf6b425de40031deb9198cdf
Jipe Mai <jipemai@MacBook-Pro-2.local>
2018-11-19 22:34:49 +0100

    feat(gallerypage): add picture for test

--- src/containers/Galerie/Galerie.js
+++ src/containers/Galerie/Galerie.js
@@ -2,25 +2,24 @@ import React from 'react'
 import { Container, Card } from 'semantic-ui-react'
 import { FormattedMessage } from 'react-intl'
 
-
-const src = '/../../assets/noebonnevial.jpg'
+const img = require('../../assets/noebonnevial.jpg')
 
 const Gallery = () => (
   <Container>
     <h2><FormattedMessage id='galerie.title' /></h2>
     <Card.Group itemsPerRow={4}>
-      <Card color='brown' image={src} />
-      <Card color='olive' image={src} />
-      <Card color='brown' image={src} />
-      <Card color='olive' image={src} />
-      <Card color='brown' image={src} />
-      <Card color='olive' image={src} />
-      <Card color='brown' image={src} />
-      <Card color='olive' image={src} />
-      <Card color='brown' image={src} />
-      <Card color='olive' image={src} />
-      <Card color='brown' image={src} />
-      <Card color='olive' image={src} />
+      <Card color='brown' image={img} />
+      <Card color='olive' image={img} />
+      <Card color='brown' image={img} />
+      <Card color='olive' image={img} />
+      <Card color='brown' image={img} />
+      <Card color='olive' image={img} />
+      <Card color='brown' image={img} />
+      <Card color='olive' image={img} />
+      <Card color='brown' image={img} />
+      <Card color='olive' image={img} />
+      <Card color='brown' image={img} />
+      <Card color='olive' image={img} />
     </Card.Group>
   </Container>
 )
