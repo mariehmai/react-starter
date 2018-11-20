3889f6b7d94ffbf869a578453662dc1e56b557bd
Jipe Mai <jipemai@MacBook-Pro-2.local>
2018-11-20 10:26:27 +0100

    feat(home): add dimmer on homepage

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,21 +1,55 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react'
-import { Grid, Image } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Container, Grid, Image, Button, Dimmer, Header } from 'semantic-ui-react'
 
 const treeImg = require('../../assets/tree.jpg')
+const machine = require('../../assets/machine.jpg')
 
-const Home = () => 
-  <div className='home'>
-      <Grid columns={2} divided>
+
+export default class Home extends Component {
+  state = {}
+
+  handleShow = () => this.setState({ active: true })
+  handleHide = () => this.setState({ active: false })
+
+  render() {
+    const { active } = this.state
+    const content = (
+      <div>
+        <h2>Exploitation agricole</h2>
+        <Button primary>Add</Button>
+        <Button>View</Button>
+      </div>
+    )
+
+    return (
+      <div className='home'>
+        <Grid columns={2} divided centered>
           <Grid.Row>
             <Grid.Column className='home-column'>
-              <Image src={treeImg} />
+              <Dimmer.Dimmable
+                as={Image}
+                dimmed={active}
+                dimmer={{ active, content }}
+                onMouseEnter={this.handleShow}
+                onMouseLeave={this.handleHide}
+                size='medium'
+                src={treeImg}
+              />
             </Grid.Column>
-            <Grid.Column>
-              <Image src={treeImg} />
+            <Grid.Column className='home-column'>
+              <Dimmer.Dimmable
+                as={Image}
+                dimmed={active}
+                dimmer={{ active, content }}
+                onMouseEnter={this.handleShow}
+                onMouseLeave={this.handleHide}
+                size='medium'
+                src={machine}
+              />
             </Grid.Column>
           </Grid.Row>
-      </Grid>
-  </div>
-
-export default Home
+        </Grid>
+      </div>
+    )
+  }
+}
