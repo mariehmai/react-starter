525f24dd02832634dd6acec0ca22eb5fffe73a67
Jipe Mai <jipemai@MacBook-Pro-2.local>
2018-11-21 21:41:41 +0100

    feat(servicespage): Modify content  presentation services page

--- src/containers/Services/Services.js
+++ src/containers/Services/Services.js
@@ -6,14 +6,14 @@ import { FormattedMessage } from 'react-intl'
 const treeImg = require('../../assets/tree.jpg')
 
 const Services = () =>
+  <Container>
+  <h2><FormattedMessage id='services.title' /></h2>
   <Grid columns={2}>
     <Grid.Row>
       <Grid.Column width={6}>
-        <Image src={treeImg} />
+        <Image src={treeImg} size='small' circular centered/>
       </Grid.Column>
       <Grid.Column width={10}>
-        <Container>
-          <h2><FormattedMessage id='services.title' /></h2>
           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
       Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
       ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
@@ -24,6 +24,13 @@ const Services = () =>
       enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
       ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
       Curabitur ullamcorper ultricies nisi.</p>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={6}>
+        <Image src={treeImg} size='small' circular centered/>
+      </Grid.Column>
+      <Grid.Column width={10}>
           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
       Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
       ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
@@ -34,9 +41,26 @@ const Services = () =>
       enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
       ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
       Curabitur ullamcorper ultricies nisi.</p>
-        </Container>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={6}>
+        <Image src={treeImg} size='small' circular centered/>
+      </Grid.Column>
+      <Grid.Column width={10}>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+      Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+      ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+      consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+      link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
+      vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+      enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
+      ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
+      Curabitur ullamcorper ultricies nisi.</p>
       </Grid.Column>
     </Grid.Row>
   </Grid>
+  </Container>
 
 export default Services
