58aefac5b9cc9ec7cbadb82f3a0b4bca241614d0
Jipe Mai <jipemai@MacBook-Pro-2.local>
2018-11-21 11:20:26 +0100

    fix(gallerypage): Rename fr to en

--- src/containers/Gallery/gallery.js
+++ src/containers/Gallery/gallery.js
@@ -6,7 +6,7 @@ const img = require('../../assets/noebonnevial.jpg')
 
 const Gallery = () => (
   <Container>
-    <h2><FormattedMessage id='galerie.title' /></h2>
+    <h2><FormattedMessage id='gallery.title' /></h2>
     <Card.Group itemsPerRow={4}>
       <Card color='brown' image={img} />
       <Card color='olive' image={img} />
