6b882f28b0d28119c2331934ec57885acb164a76
mariehmai <mai.mariehelene@gmail.com>
2018-11-27 09:24:56 +0100

    style(Services): add style in Services container

--- src/containers/Services/Services.js
+++ src/containers/Services/Services.js
@@ -1,42 +1,113 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Container, Image, Accordion, Message } from 'semantic-ui-react'
+
 import { FormattedMessage } from 'react-intl'
 
 const treeImg = require('../../assets/tree.jpg')
 
-const Services = () =>
-  <Grid columns={2}>
-    <Grid.Row>
-      <Grid.Column width={6}>
-        <Image src={treeImg} />
-      </Grid.Column>
-      <Grid.Column width={10}>
-        <Container>
-          <h2><FormattedMessage id='services.title' /></h2>
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-      Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-      ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-      consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-      link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-      vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-      enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-      ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
-      Curabitur ullamcorper ultricies nisi.</p>
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-      Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-      ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-      consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-      link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-      vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-      enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-      ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
-      Curabitur ullamcorper ultricies nisi.</p>
-        </Container>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+const panels = [
+  {
+    key: `panel-0`,
+    title: {
+      content: (
+        <div className="label-container">
+          <Image className="label-image" size="medium" rounded src={treeImg} />
+          <div className="label-description">
+            <h2 className="label-title">Service 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+          </div>
+        </div>
+      )
+    },
+    content: {
+      content: (
+        <Message
+          color="olive"
+          header="Plus d'info"
+          content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+          link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+          elementum semper nisi."
+        />
+      )
+    }
+  },
+  {
+    key: `panel-1`,
+    title: {
+      content: (
+        <div className="label-container">
+          <Image className="label-image" size="medium" rounded src={treeImg} />
+          <div className="label-description">
+            <h2 className="label-title">Service 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+          </div>
+        </div>
+      )
+    },
+    content: {
+      content: (
+        <Message
+          color="olive"
+          header="Plus d'info"
+          content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+          link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+          elementum semper nisi."
+        />
+      )
+    }
+  },
+  {
+    key: `panel-2`,
+    title: {
+      content: (
+        <div className="label-container">
+          <Image className="label-image" size="medium" rounded src={treeImg} />
+          <div className="label-description">
+            <h2 className="label-title">Service 3</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+          </div>
+        </div>
+      )
+    },
+    content: {
+      content: (
+        <Message
+          color="olive"
+          header="Plus d'info"
+          content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+          link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+          elementum semper nisi."
+        />
+      )
+    }
+  }
+]
+
+const Services = () => (
+  <Container className="services inner">
+    <h2 className="headline">
+      <FormattedMessage id="services.title" />
+    </h2>
+    <Accordion panels={panels} />
+  </Container>
+)
 
 export default Services
