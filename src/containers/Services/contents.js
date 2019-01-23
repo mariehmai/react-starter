ed1d3567dc6a326cca5672d27e8ec591ab15102b
Jipe Mai <jipemai@MacBook-Pro-2.local>
2019-01-23 19:35:25 +0100

    feat(servicespage): add new pictures

--- src/containers/Services/contents.js
+++ src/containers/Services/contents.js
@@ -1,13 +1,22 @@
 import { LabelDetail } from 'semantic-ui-react'
 
+const tractionImg = require('../../assets/traction.png')
+const landworkImg = require('../../assets/travauxsol.png')
+const seedingImg = require('../../assets/semis.png')
+const harvestImg = require('../../assets/recolte.png')
 const agriImg = require('../../assets/agricultural.png')
-const forestImg = require('../../assets/lumbering.png')
+const carriageImg = require('../../assets/transport.png')
+const harvestmachineImg = require('../../assets/abbateuse.png')
+const carrierImg = require('../../assets/porteur.png')
+const crusher300Img = require('../../assets/broyeur300cv.png')
+const crusher180Img = require('../../assets/broyeur180cv.png')
+const lowbedtruckImg = require('../../assets/porteengin.png')
 
 export default [
   {
     key: `content-0`,
     category: 'agricultural',
-    image: agriImg,
+    image: tractionImg,
     titleIntlId: 'agricultural.title1',
     messages: [
       { messageIntlId: 'agricultural.title1.item1' },
@@ -17,7 +26,7 @@ export default [
   {
     key: `content-1`,
     category: 'agricultural',
-    image: agriImg,
+    image: landworkImg,
     titleIntlId: 'agricultural.title2',
     messages: [
       { messageIntlId: 'agricultural.title2.item1' },
@@ -29,7 +38,7 @@ export default [
   {
     key: `content-2`,
     category: 'agricultural',
-    image: agriImg,
+    image: seedingImg,
     titleIntlId: 'agricultural.title3',
     messages: [
       { messageIntlId: 'agricultural.title3.item1' },
@@ -49,49 +58,49 @@ export default [
   {
     key: `content-4`,
     category: 'agricultural',
-    image: agriImg,
+    image: harvestImg,
     titleIntlId: 'agricultural.title5',
     messages: [{ messageIntlId: 'agricultural.title5.item1' }]
   },
   {
     key: `content-5`,
     category: 'agricultural',
-    image: agriImg,
+    image: carriageImg,
     titleIntlId: 'agricultural.title6',
     messages: [{ messageIntlId: 'agricultural.title6.item1' }]
   },
   {
     key: `content-6`,
     category: 'lumbering',
-    image: forestImg,
+    image: harvestmachineImg,
     titleIntlId: 'lumbering.title1',
     messages: [{ messageIntlId: 'lumbering.title1.item1' }]
   },
   {
     key: `content-7`,
     category: 'lumbering',
-    image: forestImg,
+    image: carrierImg,
     titleIntlId: 'lumbering.title2',
     messages: []
   },
   {
     key: `content-8`,
     category: 'lumbering',
-    image: forestImg,
+    image: crusher300Img,
     titleIntlId: 'lumbering.title3',
     messages: [{ messageIntlId: 'lumbering.title3.item1' }]
   },
   {
     key: `content-9`,
     category: 'lumbering',
-    image: forestImg,
+    image: crusher180Img,
     titleIntlId: 'lumbering.title4',
     messages: [{ messageIntlId: 'lumbering.title4.item1' }]
   },
   {
     key: `content-10`,
     category: 'lumbering',
-    image: forestImg,
+    image: lowbedtruckImg,
     titleIntlId: 'lumbering.title5',
     messages: [{ messageIntlId: 'lumbering.title5.item1' }]
   }
